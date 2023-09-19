a45541b1cb891d6caa5be2b6a4f7eae3b2702780
zhouyi <zhouy@zhiyun-tech.com>
2023-09-19 18:59:18 +0800

    trader-gym

--- example/gym/ray_train_bigA.py
+++ example/gym/ray_train_bigA.py
@@ -46,8 +46,8 @@ def load_data():
     # df = ak.stock_zh_a_daily("sz000625", start_date="20200101")
     # df = ak.stock_zh_a_daily("sh601318", start_date="20200101")
     # df.set_index("date")
-    df = pd.read_pickle("./data/binance-BTCUSDT-1h.pkl")
-    # df = pd.read_csv("./data/BTC_USD-Hourly.csv", parse_dates=["date"], index_col="date")
+    # df = pd.read_pickle("./data/binance-BTCUSDT-1h.pkl")
+    df = pd.read_csv("./data/BTC_USD-Hourly.csv", parse_dates=["date"], index_col="date")
     df.sort_index(inplace=True)
     df.dropna(inplace=True)
     df.drop_duplicates(inplace=True)
@@ -110,8 +110,10 @@ def create_env(config):
         df=df,
         windows=1,
         # positions=[-1, -0.5, 0, 0.5, 1],  # From -1 (=SHORT), to +1 (=LONG)
-        positions=[0, 0.5, 1],  # From -1 (=SHORT), to +1 (=LONG)
+        # positions=[0, 0.5, 1],  # From -1 (=SHORT), to +1 (=LONG)
+        positions=[0,  1],  # From -1 (=SHORT), to +1 (=LONG)
         # initial_position = 'random', #Initial position
+        dynamic_feature_functions=[],
         initial_position=0,  # Initial position
         trading_fees=0.1 / 100,  # 0.01% per stock buy / sell
         borrow_interest_rate=0,  # per timestep (= 1h here)
@@ -199,7 +201,7 @@ def train():
     stop = {
         # "training_iteration": args.stop_iters,
         "timesteps_total": 10000_0000,
-        "episode_reward_mean": 500,
+        "episode_reward_mean": 1000,
     }
 
     tuner = tune.Tuner(
@@ -228,7 +230,7 @@ from ray.rllib.algorithms.algorithm import Algorithm
 
 def test():
     # checkpoint_path = r"D:\rl\backtrader\example\gym\ray_results\PPO\PPO_TradingEnv2_1ab4e_00000_0_2023-09-13_18-34-23\checkpoint_000612"
-    checkpoint_path = r"D:\rl\backtrader\example\gym\ray_results\PPO\PPO_TradingEnv2_1d0a4_00000_0_2023-09-18_17-31-15\checkpoint_003910"
+    checkpoint_path = r"D:\rl\backtrader\example\gym\ray_results\PPO\PPO_TradingEnv2_ca408_00000_0_2023-09-15_14-47-51\checkpoint_000400"
 
     algo = Algorithm.from_checkpoint(checkpoint_path)
     env = create_env(0)
@@ -251,12 +253,12 @@ def test():
             prev_a = 0
             prev_r = 0.0
         else:
-            # pass
             state = state_out
-            # prev_a = a
-            # prev_r = reward
+            prev_a = a
+            prev_r = reward
     env.save_for_render(dir="./render_logs")
 
+# tensorboard.exe --logdir example/gym/ray_results/ppo
 if __name__ == '__main__':
-    # train()
-    test()
+    train()
+    # test()
